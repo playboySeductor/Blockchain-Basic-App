cd49b09ba6b716848606a94cf4eb85823173c508
sainik-khaddar <sainikwarror132@gmail.com>
2022-09-11 01:17:53 +0530

    Frontend Updated

---
+++ Frontend/src/components/footer/Footer.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import './footer.css'
+import nftlogo from '../../assets/logo.png'
+import { AiOutlineInstagram,AiOutlineTwitter, } from "react-icons/ai";
+import { RiDiscordFill } from "react-icons/ri";
+import { FaTelegramPlane } from "react-icons/fa";
+const Footer = () => {
+  return (
+    <div className='footer section__padding'>
+      <div className="footer-links">
+        <div className="footer-links_logo">
+        <div>
+          {/* <img src={nftlogo} alt="logo" /> */}
+          <p>NCryptot</p>
+        </div>
+        <div>
+          <h3>One Stop Solution for all your crypto trades</h3>
+        </div>
+        <div>
+         
+        </div>
+        </div>
+        <div className="footer-links_div">
+          <h4>CryptoKet</h4>
+          <p>Explore</p>
+          <p>How it Works</p>
+          <p>Counters</p>
+          <p>Contact Us</p>
+        </div>
+        <div className="footer-links_div">
+          <h4>Support</h4>
+          <p>Help center</p>
+          <p>Terms of service</p>
+          <p>Legal</p>
+          <p>Privacy policy</p>
+        </div>
+      </div>
+      <div className="footer-copyright">
+        <div>
+        <p> © {(new Date().getFullYear())} CrpytoKet, Inc. All Rights Reserved</p>
+        </div>
+        <div>
+          {/* <AiOutlineInstagram size={25} color='white' className='footer-icon' />
+          <AiOutlineTwitter size={25} color='white' className='footer-icon'/>
+          <RiDiscordFill size={25} color='white' className='footer-icon'/>
+          <FaTelegramPlane size={25} color='white'  className='footer-icon' /> */}
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ Frontend/src/components/navbar/Navbar.jsx
@@ -0,0 +1,109 @@
+import React,{ useState} from 'react'
+import './navbar.css'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import logo from '../../assets/logo.jpeg'
+import {  Link } from "react-router-dom";
+import profile from '../../pages/profile/Profile'
+import styled from 'styled-components'
+
+const Menu = () => (
+  <>
+     <Link to="/"><p>Explore</p> </Link>
+    
+     <Link to={`/profile/Rian`}><p>My Items</p></Link>
+     
+    
+  </>
+ )
+
+ const Navbar = () => {
+  const [toggleMenu,setToggleMenu] = useState(false)
+   const [user,setUser] = useState(false)
+
+  const handleLogout = () => {
+    setUser(false);
+  }
+  const handleLogin = () => {
+    setUser(true);
+  }
+
+  return (
+    <div className='navbar'>
+      <div className="navbar-links">
+        <div className="navbar-links_logo">
+          <img src={logo} alt="logo" style={{height:"3em"}} />
+          <Link to="/"> 
+            <h1>NCrypto</h1>
+          </Link>
+        </div>
+        <div className="navbar-links_container">
+          <input type="text" placeholder='Search Item Here' autoFocus={true} />
+         <Menu />
+         {user && <Link to="/"><p onClick={handleLogout}>Logout</p></Link> }
+        
+        </div>
+      </div>
+      <div className="navbar-sign">
+      {user ? (
+        <>
+         <Link to="/create"> 
+          <button type='button' className='primary-btn' >Create</button>
+        </Link>
+        <button type='button' className='secondary-btn'>Connect</button>
+        </>
+      ): (
+        <>
+        <Link to="/login"> 
+         <button type='button' className='primary-btn' onClick={handleLogin} >Sign In</button>
+        </Link>
+        <Link to="/register"> 
+          <button type='button' className='secondary-btn'>Sign Up</button>
+        </Link>
+        </>
+      )}
+       
+
+       
+      </div>
+      <div className="navbar-menu">
+        {toggleMenu ? 
+        <RiCloseLine  color="#fff" size={27} onClick={() => setToggleMenu(false)} /> 
+        : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
+        {toggleMenu && (
+          <div className="navbar-menu_container scale-up-center" >
+            <div className="navbar-menu_container-links">
+             <Menu />
+            </div>
+            <div className="navbar-menu_container-links-sign">
+            {user ? (
+              <>
+              <Link to="/create"> 
+                <button type='button' className='primary-btn' >Create</button>
+              </Link>
+              <button type='button' className='secondary-btn'>Connect</button>
+              </>
+            ): (
+              <>
+              <Link to="/login"> 
+              <button type='button' className='primary-btn' onClick={handleLogin} >Sign In</button>
+              </Link>
+              <Link to="/register"> 
+                <button type='button' className='secondary-btn'>Sign Up</button>
+              </Link>
+              </>
+            )}
+           
+            </div>
+            </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
+const Logo = styled.div`
+    font-size: 1rem;
+    font-weight: 700;
+    /* color: white; */
+`
